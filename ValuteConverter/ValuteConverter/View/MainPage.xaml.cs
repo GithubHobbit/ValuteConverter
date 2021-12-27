e0fa610d633e94e75c0e4266f93de320ea442400
aaaaa <aaaaa@DESKTOP-B2M1O6I>
2021-12-27 23:07:51 +0500

    Добавьте файлы проекта.

---
+++ ValuteConverter/ValuteConverter/View/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValuteConverter.ViewModels;
+using Xamarin.Forms;
+
+namespace ValuteConverter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+        private void OnDateSelected(object sender, DateChangedEventArgs e)
+        {
+            ((MainViewModel)BindingContext).LoadCurrencies(e.NewDate);
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(e.NewTextValue)) return;
+
+            if (!double.TryParse(e.NewTextValue, out double value))
+            {
+                ((Entry)sender).Text = e.OldTextValue;
+            }
+        }
+    }
+}
